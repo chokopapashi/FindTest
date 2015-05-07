bd00e17b385bcfacd317767b29e9f012281d63c3
chokopapashi <chokopapashi@gmail.com>
2015-05-07 16:46:15 +0900

    add line alignment function.

--- src/main/scala/DirTreeCreator.scala
+++ src/main/scala/DirTreeCreator.scala
@@ -37,10 +37,44 @@ object DirTreeCreator {
 
     def fileType(file: File): String = {
         import scala.sys.process._
-        "c:/cygwin/bin/file.exe" #| file.getAbsolutePath !!
+        (s"c:/cygwin/bin/file.exe -b ${file.getAbsolutePath}" !!).trim.split(",")(0)
     }
 
-    def fileInfo(file: File, hierarchy: String, branch: String) = hierarchy + file.getName + ":" + fileType(file)
+    val MAX_LINE_COUNT = 20
+
+    def getCharBytesLength(c: Char) = c.toString.getBytes("Shift_JIS").length
+    def getStringBytesLength(s: String) = s.getBytes("Shift_JIS").length
+
+    def splitFileName(file: File, hierarchy: List[String]) = {
+        @tailrec
+        def split(sl: List[Char], zb: Int, zmax: Int, dl: List[StringBuilder]):List[StringBuilder] = sl match {
+            case Nil => dl
+            case clh :: clt =>
+                if(zmax < (zb + getCharBytesLength(clh)))
+                    split(clt, 0, zmax, StringBuilder.newBuilder.append(clh) :: dl)
+                else {
+                    dl.head += clh
+                    split(clt, zb + getCharBytesLength(clh), zmax, dl)
+                }
+        }
+
+        /* +2 = branch size(wide char size). */
+        split(file.getName.toList, 0, MAX_LINE_COUNT-(getStringBytesLength(hierarchy.mkString)+ 2), List(StringBuilder.newBuilder)) reverse
+    }
+
+//    def fileInfo(file: File, hierarchy: String, branch: String) = hierarchy + branch + file.getName + " : (" + fileType(file) + ")"
+    def putBuffer(outBuffer: ListBuffer[String], file: File, hierarchy: List[String], branch: String, nextBranch: String) {
+        var first = true
+        for(lsb <- splitFileName(file, hierarchy)) {
+            val b = if(first) {
+                        first = false
+                        branch
+                    } else
+                        nextBranch
+
+            outBuffer += hierarchy.mkString + b + lsb.mkString
+        }
+    }
 
     /* Use view bounds */
     def init[B <% Seq[_]](l: B): B = if(l.nonEmpty) l.init.asInstanceOf[B] else l
@@ -50,14 +84,14 @@ object DirTreeCreator {
         files match {
             case Nil => /* Nothing to do */
             case FileWrap(f) :: (rofs @ List(Sentinel, _*)) => {
-                outBuffer += fileInfo(f, hierarchy.mkString, "└")
+                putBuffer(outBuffer, f, hierarchy, "└", "　")
                 f match {
                     case d if(d.isDirectory) => downDirectoryTree(listFiles(d) ::: rofs, hierarchy :+ "　", outBuffer)
                     case _ => downDirectoryTree(rofs, hierarchy, outBuffer)
                 }
             }
             case FileWrap(f) :: rofs => {
-                outBuffer += fileInfo(f, hierarchy.mkString, "├")
+                putBuffer(outBuffer, f, hierarchy, "├", "│")
                 f match {
                     case d if(d.isDirectory) => downDirectoryTree(listFiles(d) ::: rofs, hierarchy :+ "│", outBuffer)
                     case _ => downDirectoryTree(rofs, hierarchy, outBuffer)
@@ -67,24 +101,15 @@ object DirTreeCreator {
             case fs => throw new IllegalStateException(fs.toString)
         }
     }
-//    def downDirectoryTree(dir: File, hierarchy: List[String], retBuilder: StringBuilder) {
-//        dir.listFiles(dirTreeFileFilter _) match {
-//            case Array() => /* Finish */
-//            case files => files.foreach { f =>
-//                retBuilder ++= (hierarchy.mkString + 
-//                                "├" +
-//                                f.getName +
-//                                f"%n")
-//                if(f.isDirectory) {
-//                     downDirectoryTree(f, "│" :: hierarchy, retBuilder)
-//                }
-//            }
-//        }
-//    }
 
     def main(args: Array[String]) {
 
+        if(args.length == 0)
+            sys.exit(1)
+
         val root = new File(args(0))
+        if(!root.exists)
+            sys.exit(2)
 
         val treeBuffer = ListBuffer(f"${root.getName}")
         downDirectoryTree(listFiles(root), Nil, treeBuffer)
