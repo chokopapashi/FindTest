45da716205f2cc5f41a557aad84116ba11630dbd
chokopapashi <chokopapashi@gmail.com>
2015-05-11 10:09:40 +0900

    commit tentative completion.

--- src/main/scala/DirTreeCreator.scala
+++ src/main/scala/DirTreeCreator.scala
@@ -10,26 +10,65 @@ import java.time.ZoneId
 import java.time.format.DateTimeFormatter
 
 import scala.annotation.tailrec
-import scala.collection.immutable.Set
-import scala.collection.immutable.Stack
-import scala.collection.mutable.ListBuffer
+import scala.collection.mutable
+import scala.collection.JavaConversions._
 import scala.language.implicitConversions
 import scala.language.postfixOps
 import scala.math
 
 object DirTreeCreator {
 
+    implicit def toFileFilter(func: (File) => Boolean) = new FileFilter {
+        def accept(file: File): Boolean = func(file)
+    }
+
     val MAX_LINE_BYTE_WIDTH = 50
 
-//    def fileType(file: File): String = {
-//        import scala.sys.process._
-//        (s"c:/cygwin/bin/file.exe -b ${file.getAbsolutePath}" !!).trim.split(",")(0)
-//    }
+    def fileAttributes_fileType(file: File): String = {
+        import scala.sys.process._
+        (s"c:/cygwin/bin/file.exe -b ${file.getAbsolutePath}" !!).trim.split(",")(0)
+    }
 
-    def fileAttributes(file: File): String = {
-        val sb = StringBuilder.newBuilder
-        val supportedAttributes = FileSystems.getDefault.supportedFileAttributeViews
+    def fileAttributes_dateTime(file: File, supportedAttributes: mutable.Set[String]): String = {
         if(supportedAttributes.contains("basic")) {
+            val sb = StringBuilder.newBuilder
+
+            val attrb = Files.readAttributes(file.toPath, classOf[BasicFileAttributes]) 
+            def fileTime2ZoneDateTime(fileTime: FileTime) = fileTime.toInstant.atZone(ZoneId.systemDefault)
+            val dtf = DateTimeFormatter.ofPattern("yy/MM/dd-HH:mm:ss")
+
+            sb ++= fileTime2ZoneDateTime(attrb.creationTime).format(dtf) + " "
+            sb ++= fileTime2ZoneDateTime(attrb.lastModifiedTime).format(dtf) + " "
+            sb ++= fileTime2ZoneDateTime(attrb.lastAccessTime).format(dtf) + " "
+
+            sb.mkString
+        } else
+            "--/--/-----:--:-- --/--/-----:--:-- --/--/-----:--:--"
+    }
+
+    def fileAttributes_dos(file: File, supportedAttributes: mutable.Set[String]): String = {
+        if(supportedAttributes.contains("dos")) {
+            val sb = StringBuilder.newBuilder
+
+            val attrd = Files.readAttributes(file.toPath, classOf[DosFileAttributes]) 
+            if(attrd.isDirectory) {
+                sb ++= "----"
+            } else {
+                sb += (if(attrd.isArchive())  'A' else '-')
+                sb += (if(attrd.isHidden())   'H' else '-')
+                sb += (if(attrd.isReadOnly()) 'R' else '-')
+                sb += (if(attrd.isSystem())   'S' else '-')
+            }
+
+            sb.mkString
+        } else
+            "----"
+    }
+
+    def fileAttributes_size(file: File, supportedAttributes: mutable.Set[String]): String = {
+        if(supportedAttributes.contains("basic")) {
+            val sb = StringBuilder.newBuilder
+
             val GIGA = 1073741824L
             val MEGA = 1048576L
             val KIRO = 1024L
@@ -51,37 +90,23 @@ object DirTreeCreator {
                 else
                     sb ++= f"${attrb.size}%8d"
             }
-        } else {
-            sb ++= "       -"
-        }
 
+            sb.mkString
+        } else
+            "       -"
+    }
+
+    def fileAttributes(file: File): String = {
+        val sb = StringBuilder.newBuilder
+        val supportedAttributes = FileSystems.getDefault.supportedFileAttributeViews
+
+        sb ++= fileAttributes_size(file, supportedAttributes)
         sb ++= " "
-
-        if(supportedAttributes.contains("dos")) {
-            val attrd = Files.readAttributes(file.toPath, classOf[DosFileAttributes]) 
-            if(attrd.isDirectory) {
-                sb ++= "----"
-            } else {
-                sb += (if(attrd.isArchive())  'A' else '-')
-                sb += (if(attrd.isHidden())   'H' else '-')
-                sb += (if(attrd.isReadOnly()) 'R' else '-')
-                sb += (if(attrd.isSystem())   'S' else '-')
-            }
-        } else {
-            sb ++= "----"
-        }
-
+        sb ++= fileAttributes_dos(file, supportedAttributes)
         sb ++= " "
-
-        if(supportedAttributes.contains("basic")) {
-            val attrb = Files.readAttributes(file.toPath, classOf[BasicFileAttributes]) 
-            def fileTime2ZoneDateTime(fileTime: FileTime) = fileTime.toInstant.atZone(ZoneId.systemDefault)
-            val dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd-HH:mm:ss")
-
-            sb ++= fileTime2ZoneDateTime(attrb.creationTime).format(dtf) + " "
-            sb ++= fileTime2ZoneDateTime(attrb.lastModifiedTime).format(dtf) + " "
-            sb ++= fileTime2ZoneDateTime(attrb.lastAccessTime).format(dtf) + " "
-        }
+        sb ++= fileAttributes_dateTime(file, supportedAttributes)
+        sb ++= " "
+//        sb ++= fileAttributes_fileType(file)
 
         sb.mkString
     }
@@ -93,7 +118,7 @@ object DirTreeCreator {
 //    def getStringBytesLength(s: String) = s.getBytes("Shift_JIS").length
 //    def newBytes() = Array.fill(MAX_LINE_BYTE_WIDTH)(0x20.toByte)
 
-    def alignWidth(src: ListBuffer[Byte], pw: Int): String = {
+    def alignWidth(src: mutable.ListBuffer[Byte], pw: Int): String = {
         val cw = pw + src.size
         if(cw < MAX_LINE_BYTE_WIDTH)
             src ++= List.fill(MAX_LINE_BYTE_WIDTH - cw)(0x20.toByte)
@@ -104,23 +129,23 @@ object DirTreeCreator {
         val pw = hierarchy.mkString.getBytes.length + 2    /* +2 = branch size(wide char size). */
 
         @tailrec
-        def split(src: List[Char], tmp: ListBuffer[Byte], dst: List[String]):List[String] = src match {
+        def split(src: List[Char], tmp: mutable.ListBuffer[Byte], dst: List[String]):List[String] = src match {
             case Nil => if(tmp.isEmpty) dst
                         else alignWidth(tmp, pw) :: dst
             case srch :: srct => {
                 val bytes = getCharBytes(srch)
                 if(MAX_LINE_BYTE_WIDTH < (pw + tmp.size + bytes.length)) {
-                    split(srct, ListBuffer(bytes: _*), alignWidth(tmp, pw) :: dst)
+                    split(srct, mutable.ListBuffer(bytes: _*), alignWidth(tmp, pw) :: dst)
                 } else {
                     split(srct, (tmp ++= bytes), dst)
                 }
             }
         }
 
-        split(file.getName.toList, ListBuffer.empty[Byte], Nil).reverse
+        split(file.getName.toList, mutable.ListBuffer.empty[Byte], Nil).reverse
     }
 
-    def setupFileInfo(dsts: ListBuffer[String], file: File, hierarchy: List[String], branch: String, nextBranch: String) {
+    def setupFileInfo(dsts: mutable.ListBuffer[String], file: File, hierarchy: List[String], branch: String, nextBranch: String) {
         var first = true
         for(fn <- splitFileName(file, hierarchy)) {
             if(first) {
@@ -131,7 +156,7 @@ object DirTreeCreator {
         }
     }
 
-    def setupRootInfo(dsts: ListBuffer[String], file: File) {
+    def setupRootInfo(dsts: mutable.ListBuffer[String], file: File) {
         var first = true
         for(fn <- splitFileName(file, Nil)) {
             if(first) {
@@ -155,10 +180,6 @@ object DirTreeCreator {
         case _ => false
     }
 
-    implicit def toFileFilter(func: (File) => Boolean) = new FileFilter {
-        def accept(file: File): Boolean = func(file)
-    }
-
     trait FileHolder
     object Sentinel extends FileHolder
     case class FileWrap(file: File) extends FileHolder
@@ -170,7 +191,7 @@ object DirTreeCreator {
     def init[B <% Seq[_]](l: B): B = if(l.nonEmpty) l.init.asInstanceOf[B] else l
 
     @tailrec
-    def downDirectoryTree(files: List[FileHolder], hierarchy: List[String], dsts: ListBuffer[String]) {
+    def downDirectoryTree(files: List[FileHolder], hierarchy: List[String], dsts: mutable.ListBuffer[String]) {
         files match {
             case Nil => /* Nothing to do */
             case FileWrap(f) :: (rofs @ List(Sentinel, _*)) => {
@@ -201,10 +222,10 @@ object DirTreeCreator {
         if(!root.exists)
             sys.exit(2)
 
-        println("File/Folder")
-        println("------------------------------------------------------------------------------")
+        println("File/Folder                                        Size     Attr Creation          LastModified      LastAccess       ")
+        println("--------------------------------------------------+--------+----+-----------------+-----------------+-----------------")
 
-        val treeBuffer = ListBuffer.empty[String]
+        val treeBuffer = mutable.ListBuffer.empty[String]
         setupRootInfo(treeBuffer,root)
         downDirectoryTree(listFiles(root), Nil, treeBuffer)
         println(treeBuffer.mkString(f"%n"))
